edeabbd588b538d174c5d6be5a21c1f964edd430
Siyona-13 <immievashishth@gmail.com>
2025-01-26 20:54:12 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -225,7 +225,7 @@ app.post("/search-face", async (req, res) => {
 });
 
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
